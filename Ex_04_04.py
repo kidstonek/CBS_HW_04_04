85e66b4c93e7c5e4db4f0416dd42d321971c4400
kidstonek <96789294+kidstonek@users.noreply.github.com>
2024-10-17 18:22:24 +0300

    start 4.1

--- Ex_04_04.py
+++ Ex_04_04.py
@@ -9,7 +9,7 @@ import sqlite3
 import random
 conn = sqlite3.connect('ex04.sqlite3')
 
-# cursor = conn.cursor()
+cursor = conn.cursor()
 
 def create_table():
     query = '''CREATE TABLE IF NOT EXISTS expenses(
@@ -17,23 +17,33 @@ def create_table():
     destination VARCHAR(50),
     expenses REAL,
     time DATE,
-    profit INTEGER
+    profit REAL
     );
     '''
     cursor.execute(query)
 
 
-def add_expenses(destination: str, amount: float, time: str):
-    query = '''INSERT INTO expenses (destination, amount, time) VALUES (?, ?, ?);'''
-    cursor.execute(query, [destination, amount, time])
+def add_expenses(destination: str, expenses: float, time: str, income: float):
+    query = '''INSERT INTO expenses (destination, expenses, time, profit) VALUES (?, ?, ?, ?);'''
+    cursor.execute(query, [destination, expenses, time, income])
     conn.commit()
 
 
-random.choice(['drugs', 'movies', 'taxi', 'toys'])
+random.choice(['drugs', 'movies', 'taxi', 'toys', 'games'])
 round(random.uniform(10, 3000), 2)
 
 if __name__ == '__main__':
-    # create_table()
+    create_table()
+    print('do you want to fill the db?')
+    usr_in = int(input('1 - to fill db, other will leave it blank '))
+    if usr_in == 1:
+        times = int(input("how many rows do you need?"))
+        for i in range(times):
+            expenses_for = random.choice(['drugs', 'movies', 'taxi', 'toys', 'games'])
+            expenses = round(random.uniform(10, 3000), 2)
+            income = round(random.uniform(10, 3000), 2)
+            expenses_date = f'{random.randint(1991, 2024)}-{random.randint(1, 12)}-{random.randint(1, 31)}'
+            add_expenses(expenses_for, expenses, expenses_date, income)
 
-    for i in range(10):
-        print(random.choice(['drugs', 'movies', 'taxi', 'toys']), round(random.uniform(10, 3000), 2))
+
+
