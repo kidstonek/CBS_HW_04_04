eb7a2cf7e3896900769a5f92ff37551d7e159379
kidstonek <96789294+kidstonek@users.noreply.github.com>
2024-10-18 17:08:47 +0300

    some

--- Ex_04_04.py
+++ Ex_04_04.py
@@ -8,6 +8,7 @@ i прибуткiв за місяць.
 
 import sqlite3
 import random
+
 conn = sqlite3.connect('ex04.sqlite3')
 
 cursor = conn.cursor()
@@ -28,6 +29,18 @@ def add_expenses(destination: str, expenses: float, time: str, income: float):
     query = '''INSERT INTO expenses (destination, expenses, time, profit) VALUES (?, ?, ?, ?);'''
     cursor.execute(query, [destination, expenses, time, income])
     conn.commit()
+
+
+def search_expenses(needed_period: str):
+    query = f'''SELECT 
+    SUM(expenses) AS total_expenses, 
+    SUM(profit) AS total_profit, 
+    (SUM(expenses) - SUM(profit)) AS tot
+    FROM expenses e 
+    WHERE time LIKE ?;
+    '''
+    result = cursor.execute(query, [needed_period])
+    return result.fetchall()
 
 
 def expenses_date_():
@@ -54,6 +67,20 @@ if __name__ == '__main__':
         print('the data was added')
     while True:
         print('if you want to see type:\n1 - expenses and income for exect period')
-
+        usr_input = input("what you decide?: ")
+        if usr_input == '1':
+            usr_year = input('input needed year (XXXX): ')
+            usr_month = input('input number for the month: ')
+            if int(usr_month) < 10:
+                usr_month = '0' + usr_month
+            my_question_time = f"{usr_year}%-{usr_month}-%"
+            answer = search_expenses(my_question_time)
+            print(answer[0])
+            print(f'In year {usr_year} and {usr_month} your balance was: income {answer[0][1]}, you have spent {answer[0][0]}')
+            print(f'In TOTAL: {answer[0][2]}')
+            input('....')
+        else:
+            print('bye')
+            break
 
 
--- Ex_04_05.py
+++ Ex_04_05.py
@@ -11,6 +11,10 @@ id (INT PRIMARY KEY) - 1, 2, 3, ... currency_name (TEXT) - UAH currency_value (R
 import requests
 import datetime
 from bs4 import BeautifulSoup
+import sqlite3
+
+
+#Looking for the currency codes
 
 url = 'https://www.iban.com/currency-codes'
 response = requests.get(url)
@@ -24,21 +28,67 @@ for cur in dd.find_all('tr'):
   my_currencies[cur.find_all('td')[3].text] = {'country': str(cur.find_all('td')[0].text), 'currency_id': str(cur.find_all('td')[2].text), 'currency_code':  str(cur.find_all('td')[3].text)}
 
 
+# Monobank API in use
+
+conn = sqlite3.connect('ex05.sqlite3')
+
+cursor = conn.cursor()
+
+def create_table():
+    query = '''CREATE TABLE IF NOT EXISTS exchange(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    currency1_name TEXT,
+    currency2_name TEXT,
+    currency_value REAL,
+    time REAL
+    );
+    '''
+    cursor.execute(query)
+
+create_table()
+
+def add_exchange(cur1: str, cur2: str, currency_value: float, times: str):
+    query = '''INSERT INTO exchange(currency1_name, currency2_name, currency_value, time) VALUES (?, ?, ?, ?);'''
+    cursor.execute(query, [cur1, cur2, currency_value, times])
+    conn.commit()
+
 url = 'https://api.monobank.ua/bank/currency'
 
-
-
-
 result = requests.get(url)
+
+def currency_code(sample:int):
+    if len(str(sample)) == 1:
+        sample = '00' + str(sample)
+    if len(str(sample)) == 2:
+        sample = '0' + str(sample)
+    return sample
+# {'currencyCodeA': 826, 'currencyCodeB': 980, 'date': 1729257616, 'rateCross': 54.0408}
+# for i in xxz:
+#     if i.get('rateBuy'):
+#         print('yes')
+#     else:
+#         print('no ratebuy')
 # {'currencyCodeA': 840, 'currencyCodeB': 980, 'date': 1729112473, 'rateBuy': 41.05, 'rateSell': 41.4852}
 for exchange in result.json():
     print(exchange)
-    cur_code = exchange['currencyCodeA']
-    if len(str(cur_code)) == 1:
-        cur_code = '00' + str(cur_code)
-    if len(str(cur_code)) == 2:
-        cur_code = '0' + str(cur_code)
+    cur_code1 = currency_code(exchange['currencyCodeA'])
+    cur_code2 = currency_code(exchange['currencyCodeB'])
     try:
-        print(my_currencies.get(str(cur_code))['currency_id'])
+        print(my_currencies.get(str(cur_code1))['currency_id'])
+        cur_code_1 = my_currencies.get(str(cur_code1))['currency_id']
     except Exception as e:
         print(e, 'NAN')
+        cur_code_1 = 'NAN'
+    try:
+        print(my_currencies.get(str(cur_code1))['currency_id'])
+        cur_code_2 = my_currencies.get(str(cur_code2))['currency_id']
+    except Exception as e:
+        print(e, 'NAN')
+        cur_code_2 = 'NAN'
+    if exchange.get('rateBuy'):
+        cur_val = exchange.get('rateBuy')
+    else:
+        cur_val = exchange.get('rateCross')
+    add_exchange(cur_code1,cur_code2, cur_val, exchange.get('date'))
+
+
